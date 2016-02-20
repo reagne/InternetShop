c224fe37b418abd3aef35d4fda947aff80c11e67
adrozdek <agata.drozdek@gmail.com>
2016-02-20 11:59:09 +0100

    Adding DB with a few users and one admin. Updating things to do

--- InternetShop.sql
+++ InternetShop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas wygenerowania: 19 Lut 2016, 14:52
+-- Czas wygenerowania: 20 Lut 2016, 10:57
 -- Wersja serwera: 5.5.47-0ubuntu0.14.04.1
 -- Wersja PHP: 5.5.9-1ubuntu4.14
 
@@ -32,7 +32,14 @@ CREATE TABLE IF NOT EXISTS `Admins` (
   `password` varchar(70) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
+
+--
+-- Zrzut danych tabeli `Admins`
+--
+
+INSERT INTO `Admins` (`id`, `email`, `password`) VALUES
+(1, 'agata@admin.pl', '$2y$11$JpWqU8Vwox/WmvImtlb59eNwUsO4hpkqBRDiT9kvFqOIUBAek2jm6');
 
 -- --------------------------------------------------------
 
@@ -110,7 +117,16 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `address` varchar(150) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Zrzut danych tabeli `Users`
+--
+
+INSERT INTO `Users` (`id`, `first_name`, `last_name`, `email`, `password`, `address`) VALUES
+(1, 'Ela', 'Nowik', 'ela@wp.pl', '$2y$11$zAv5FSXNFG4h5dPe4EZhOOxgf2d32Q/PwKHaz8QA09XKeat11lRiO', 'ul. Jagienki 6\r\n45-300 Kraków'),
+(2, 'Anna', 'Wezyr', 'anna@gmail.pl', '$2y$11$4uRtnef8OogqtdiLrTMiL.rjB4JgyEsEFVFFM6mSVi61F8Yr0t/Fi', 'ul. Sosnowa 23\r\n67-100 Nowa Sól'),
+(3, 'Andrzej', 'Koala', 'andrzej@wp.pl', '$2y$11$BTDLr2J8MOzz7RApSPZ4D.BIgwwZ6YDGTH1iq0P/hLGt0fyf6D0XS', 'ul. Opolska 2\r\n34-100 O³awa');
 
 --
 -- Ograniczenia dla zrzutów tabel
